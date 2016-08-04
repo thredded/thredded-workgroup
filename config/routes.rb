493ef3959ec2471a95c69679402abf6f38b6115a
Tim Diggins <tim@red56.co.uk>
2016-08-04 13:01:11 +0100

    fix deprecation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 Thredded::PersonalizedNavigation::Engine.routes.draw do
   controller "navs" do
-    get "unread", to: :unread, as: :unread_nav
+    get "unread", action: :unread, as: :unread_nav
   end
 
   mount Thredded::Engine => ""
