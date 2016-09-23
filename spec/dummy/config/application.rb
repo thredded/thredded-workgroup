827765c489a1a86629788da1de331ef9452251b2
Tim Diggins <tim@red56.co.uk>
2016-09-23 07:38:13 +0100

    use Thredded's migration files

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -28,6 +28,9 @@ module Dummy
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
+    # Add migrate path from Thredded. Could also put the dummy paths into here as well
+    config.paths['db/migrate'] << "#{Gem.loaded_specs['thredded'].full_gem_path}/db/migrate"
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
