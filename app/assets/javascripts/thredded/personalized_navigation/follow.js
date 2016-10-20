527e2b02f5d6e3945f07e7646bb2e84732517aeb
Ella Schofield <ella@red56.uk>
2016-10-20 14:38:28 +0100

    updating to latest thredded and fixing changes (#13)

--- app/assets/javascripts/thredded/personalized_navigation/follow.js
+++ app/assets/javascripts/thredded/personalized_navigation/follow.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
   function updateFollowStatus($svg, $topic, followStatus) {
     var $newSvg = $svg.clone();
     if (followStatus) {
@@ -10,24 +9,25 @@ $(document).ready(function () {
       $topic.removeClass('thredded--topic-following').addClass('thredded--topic-notfollowing')
       $newSvg.find('use').attr('xlink:href', '#thredded-unfollow-icon');
     }
+    $newSvg.click(clickFollowIcon);
     $svg.replaceWith($newSvg);
     $newSvg.show();
   };
-
-  $('.thredded--topic-follow-icon').click(function () {
-    var $svg = $(this).find('svg');
-    var $topic = $svg.parents('.thredded--topics--topic');
+  function clickFollowIcon(){
+    var $icon = $(this);
+    var $topic = $icon.parents('.thredded--topics--topic');
     var following = $topic.hasClass('thredded--topic-following');
     var topicId = $topic.data("topic");
     var messageboardId = $topic.data("messageboard");
     var rootPath = $('#thredded--container').data('thredded-root-url');
 
     var path = "" + rootPath + messageboardId + "/" + topicId + "/" + (following ? 'unfollow.json' : 'follow.json');
-    $svg.show();
+    $icon.show();
     $.ajax({
       url: path, type: "POST", success: function (data) {
-        updateFollowStatus($svg, $topic,  data["follow"])
+        updateFollowStatus($icon, $topic,  data["follow"])
       }
     });
-  });
+  }
+  $('.thredded--topics--follow-icon').click(clickFollowIcon);
 });
