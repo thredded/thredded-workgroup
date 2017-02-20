4132e0b4189f0ce2d53d8ee397c19442040a253e
Tim Diggins <tim@red56.uk>
2017-02-20 12:52:04 +0000

    fix problem when setting up dummy db (#21)

--- spec/dummy/app/assets/javascripts/application.js
+++ spec/dummy/app/assets/javascripts/application.js
@@ -4,5 +4,8 @@
 //= require thredded/dependencies
 //= require locales/jquery.timeago.pt-br
 //= require thredded/thredded
+
+// # IMPORTANT: Mandatory for Thredded::Workgroup dummy
 //= require thredded-workgroup
+
 //= require_tree ./app
