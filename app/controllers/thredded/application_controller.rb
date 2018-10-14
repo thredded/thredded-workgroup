e1d26332d8c33dbd343a287f533524edd3d28dcb
Tim Diggins <tim@red56.uk>
2018-10-14 15:45:36 +0100

    rubocop

--- app/controllers/thredded/application_controller.rb
+++ app/controllers/thredded/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-require_dependency File.expand_path("../../app/controllers/thredded/application_controller", Thredded::Engine.called_from)
+require_dependency File.expand_path("../../app/controllers/thredded/application_controller",
+                                    Thredded::Engine.called_from)
 
 module Thredded
   class ApplicationController
