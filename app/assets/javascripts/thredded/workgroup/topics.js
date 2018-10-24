7813c4d1c1059bfc4f538b5dc700ebf45aa382b9
Tim Diggins <tim@red56.uk>
2018-10-24 16:09:26 +0100

    simplify rollovers to make unfollow more useful.

--- app/assets/javascripts/thredded/workgroup/topics.js
+++ app/assets/javascripts/thredded/workgroup/topics.js
@@ -1,7 +1,6 @@
 (function($){
   $(function () {
-    $('.thredded--topics--topic .thredded--topic-title, .thredded--topics--topic .thredded--topic-followers,' +
-      '.thredded--topics--topic .thredded--topics--posts-count').hover(
+    $('.thredded--topics--topic .thredded--topic-title, .thredded--topics--topic .thredded--topics--posts-count').hover(
       function(){$(this).closest('.thredded--topics--topic').addClass("thredded--rollover")},
       function(){$(this).closest('.thredded--topics--topic').removeClass("thredded--rollover")}
     ).click(function() {
