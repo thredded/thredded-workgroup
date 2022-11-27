4aa2a182295704efae7f8a68ff66d480adb2d777
Tim Diggins <tim@red56.uk>
2022-11-27 19:47:28 +0000

    fix for routing changes in thredded main (#44)
    This makes also routing much simpler by removing the
    proxy delegators.

--- app/controllers/thredded/posts_controller.rb
+++ app/controllers/thredded/posts_controller.rb
@@ -33,12 +33,13 @@ module Thredded
       if params[:post_referer].present?
         redirect_to params[:post_referer], notice: generate_flash_for(post)
       else
-        redirect_to unread_nav_path, notice: generate_flash_for(post)
+        redirect_to thredded_workgroup.unread_nav_path, notice: generate_flash_for(post)
       end
     end
 
     def generate_flash_for(post)
-      path_to_post = messageboard_topic_path(parent_topic.messageboard, parent_topic, anchor: "post_#{post.id}")
+      path_to_post = thredded.messageboard_topic_path(parent_topic.messageboard, parent_topic,
+                                                      anchor: "post_#{post.id}")
       # rubocop:disable Rails/OutputSafety
       "Successfully replied to #{view_context.link_to(parent_topic.title, path_to_post)}".html_safe
       # rubocop:enable Rails/OutputSafety
